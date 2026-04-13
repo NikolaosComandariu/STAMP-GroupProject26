ea9fa860a14c910309adc3cd8dc7dc3736ddecfb
Nikolaos Comandariu <s283743@uos.ac.uk>
2026-04-13 14:29:25 +0100

    CriteriaButtons: Create class for CriteriaButtonManager

---
+++ Source/MyProject/Private/CriteriaButtonManager.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CriteriaButtonManager.h"
+
+CriteriaButtonManager::CriteriaButtonManager()
+{
+}
+
+CriteriaButtonManager::~CriteriaButtonManager()
+{
+}
