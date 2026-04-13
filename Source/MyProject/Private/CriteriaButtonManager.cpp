40838c632454c7d2e5562b6a67b5eb76c04cbee7
Nikolaos Comandariu <s283743@uos.ac.uk>
2026-04-13 15:20:10 +0100

    Feat (CriteriaManager): Create sphere with c++ script component

--- Source/MyProject/Private/CriteriaButtonManager.cpp
+++ Source/MyProject/Private/CriteriaButtonManager.cpp
@@ -1,12 +1,28 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "CriteriaButtonManager.h"
 
-CriteriaButtonManager::CriteriaButtonManager()
+// Sets default values for this component's properties
+UCriteriaButtonManager::UCriteriaButtonManager()
 {
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
 }
 
-CriteriaButtonManager::~CriteriaButtonManager()
+// Called when the game starts
+void UCriteriaButtonManager::BeginPlay()
 {
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+// Called every frame
+void UCriteriaButtonManager::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
 }
--- Source/MyProject/Private/CriteriaButtonManager.h
+++ Source/MyProject/Private/CriteriaButtonManager.h
@@ -3,13 +3,26 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "CriteriaButtonManager.generated.h"
 
-/**
- * 
- */
-class CriteriaButtonManager
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class UCriteriaButtonManager : public UActorComponent
 {
-public:
-	CriteriaButtonManager();
-	~CriteriaButtonManager();
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UCriteriaButtonManager();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
 };
